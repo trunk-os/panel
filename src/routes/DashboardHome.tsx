e002a0139b8607421531fac88ee31365c086b469
Erik Hollensbe <git@hollensbe.org>
2025-10-06 19:44:40 -0700

    let there be links
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/routes/DashboardHome.tsx
+++ src/routes/DashboardHome.tsx
@@ -1,3 +1,17 @@
+import { NavLink } from "react-router";
+
+import CenterForm from "../components/CenterForm.tsx";
+
 export default function DashboardHome() {
-  return <div>Welcome to the Dashboard!</div>;
+  return (
+    <CenterForm>
+      <div>Welcome to the Dashboard!</div>
+      <div>
+        <NavLink to="/dashboard/log">Audit Log</NavLink>
+      </div>
+      <div>
+        <NavLink to="/dashboard/user">User Management</NavLink>
+      </div>
+    </CenterForm>
+  );
 }
