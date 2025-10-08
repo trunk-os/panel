d5f2baa383838788041c2028e65e733d38cc4557
Erik Hollensbe <git@hollensbe.org>
2025-10-08 04:14:03 -0700

    still a lot of things to polish but it's mostly working
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/routes/DiskManagement.tsx
+++ src/routes/DiskManagement.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import IconButton from "@mui/material/IconButton";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
 import Button from "@mui/material/Button";
-import AddIcon from "@mui/icons-material/Add";
-import DeleteIcon from "@mui/icons-material/Delete";
+import Alert from "@mui/material/Alert";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import TextField from "@mui/material/TextField";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import Table from "../components/Table.tsx";
 import ConfirmDialog from "../components/ConfirmDialog.tsx";
@@ -13,21 +19,131 @@ import defaultClient from "../lib/client.ts";
 import { periodicCallWithState } from "../lib/effects.ts";
 import { toGB } from "../lib/units.ts";
 
+async function modifyDisk({ name, create, kind, size }) {
+  const client = defaultClient();
+
+  switch (kind) {
+    case "Dataset": {
+      if (create) {
+        return await client.zfs_create_dataset(name, size > 0 ? size : null);
+      } else {
+        return await client.zfs_modify_dataset(
+          name,
+          name,
+          size > 0 ? size : null
+        );
+      }
+      break;
+    }
+    case "Volume": {
+      if (create) {
+        return await client.zfs_create_volume(name, size);
+      } else {
+        return await client.zfs_modify_volume(name, name, size);
+      }
+      break;
+    }
+  }
+}
+
+function DiskProperties(props) {
+  let [editDiskStatus, setEditDiskStatus] = React.useState({
+    status: null,
+    error_detail: {},
+  });
+
+  return (
+    <form
+      id="edit-disk-form"
+      autoComplete="off"
+      onSubmit={(event) => {
+        modifyDisk({
+          name: event.target.elements.name.value,
+          create: props.create,
+          kind: props.kind,
+          size: parseInt(event.target.elements.size.value),
+        }).then((response) => {
+          props.setDiskProperties({ open: false });
+          setEditDiskStatus({
+            status: response.ok,
+            error_detail: response.error_detail,
+          });
+        });
+        event.preventDefault();
+      }}
+    >
+      {editDiskStatus.status !== null ? (
+        <>
+          <div className="login-item">
+            <Alert severity={editDiskStatus.status ? "success" : "error"}>
+              {editDiskStatus.status
+                ? "Disk " +
+                  (props.create ? "Created" : "Modified") +
+                  " Successfully."
+                : `[ERROR]: ${editDiskStatus.error_detail.title}: ${editDiskStatus.error_detail.detail}`}
+            </Alert>
+          </div>
+          <div style={{ height: "1em" }}></div>
+        </>
+      ) : (
+        <></>
+      )}
+      <div className="login-item">
+        <TextField
+          style={{ width: "100%" }}
+          label="Disk Name"
+          id="name"
+          defaultValue={props.disk ? props.disk.name : null}
+          disabled={!props.create}
+        />
+      </div>
+      <div style={{ height: "1em" }}></div>
+      <div className="login-item">
+        <TextField
+          style={{ width: "100%" }}
+          label="Size (in bytes)"
+          id="size"
+        />
+      </div>
+      <div style={{ height: "2em" }}></div>
+      <div className="login-item">
+        <Button style={{ width: "100%" }} variant="contained" type="submit">
+          Modify Disk
+        </Button>
+      </div>
+    </form>
+  );
+}
+
 export default function DiskManagement(props) {
   let [zfsFilter, setZfsFilter] = React.useState(null);
   let [zfsList, setZfsList] = React.useState([]);
   let [menuInfo, setMenuInfo] = React.useState({ status: false });
   let [confirmName, setConfirmName] = React.useState(null);
+  let [editDisk, setEditDisk] = React.useState({ open: false });
 
   periodicCallWithState("zfs_list", setZfsList, {
     args: zfsFilter,
     defaultState: [],
   });
 
-  zfsList.sort((a, b) => a.name > b.name);
+  zfsList.sort((a, b) => (a.name > b.name ? 1 : a.name === b.name ? 0 : -1));
 
   return (
     <div>
+      <Dialog open={editDisk.open}>
+        <DialogTitle>
+          {(editDisk.create ? "Create" : "Modify") + " Disk"}
+        </DialogTitle>
+        <DialogContent>
+          <DiskProperties
+            setDiskProperties={setEditDisk}
+            create={editDisk.create}
+            kind={editDisk.kind}
+            disk={{ name: editDisk.name }}
+          />
+        </DialogContent>
+      </Dialog>
       <ConfirmDialog
         open={confirmName}
         title="Confirm Volume Deletion?"
@@ -66,6 +182,7 @@ export default function DiskManagement(props) {
             >
               <MenuItem
                 onClick={() => {
+                  setEditDisk({ open: true, create: true, kind: "Dataset" });
                   setMenuInfo({ status: false });
                 }}
               >
@@ -73,6 +190,7 @@ export default function DiskManagement(props) {
               </MenuItem>
               <MenuItem
                 onClick={() => {
+                  setEditDisk({ open: true, create: true, kind: "Volume" });
                   setMenuInfo({ status: false });
                 }}
               >
@@ -95,7 +213,21 @@ export default function DiskManagement(props) {
           name: (x) => <div style={{ textAlign: "justify" }}>{x}</div>,
           size: toGB,
           used: toGB,
-          modify: (_, record) => <Button variant="outlined">Modify</Button>,
+          modify: (_, record) => (
+            <Button
+              variant="outlined"
+              onClick={() => {
+                setEditDisk({
+                  open: true,
+                  create: false,
+                  name: record.name,
+                  kind: record.kind,
+                });
+              }}
+            >
+              Modify
+            </Button>
+          ),
           delete: (_, record) => (
             <IconButton
               color="error"
