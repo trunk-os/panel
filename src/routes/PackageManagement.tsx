b922b2f2e4836d8f8919438fc7b5b78f2e5b555a
Erik Hollensbe <git@hollensbe.org>
2025-10-08 17:55:35 -0700

    some dents; checkpointing for now
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/routes/PackageManagement.tsx
+++ src/routes/PackageManagement.tsx
@@ -1,3 +1,16 @@
+import React from "react";
+
+import defaultClient from "../lib/client.ts";
+import { periodicCallWithState } from "../lib/effects.ts";
+
 export default function PackageManagement(props) {
+  let [packageList, setPackageList] = React.useState([]);
+
+  periodicCallWithState("list_packages", setPackageList, {
+    defaultState: [],
+  });
+
+  console.log(packageList);
+
   return <div />;
 }
